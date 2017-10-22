fe64a7149afec870963576d8d5afb340783aed08
polett-pizarro <polett.pizarro@gmail.com>
2017-10-22 00:49:37 -0300

    actualizacion consultas

--- Crinfo-Spring/src/main/java/cl/citiaps/spring/backend/repository/TweetRepository.java
+++ Crinfo-Spring/src/main/java/cl/citiaps/spring/backend/repository/TweetRepository.java
@@ -12,32 +12,32 @@ import cl.citiaps.spring.backend.entities.Tweet;
 
 public interface TweetRepository extends PagingAndSortingRepository<Tweet, Integer> {
 	@Query(value = "SELECT commune.name_commune, count(tweet.id_tweet) FROM tweet, commune  " +
-			"WHERE tweet.id_commune = commune.id_commune " +
-			"GROUP BY tweet.id_commune " +
-			"ORDER BY count(tweet.id_tweet) desc " +
-			"LIMIT 5", nativeQuery=true)
+		"WHERE tweet.id_commune = commune.id_commune " +
+		"GROUP BY tweet.id_commune " +
+		"ORDER BY count(tweet.id_tweet) desc " +
+		"LIMIT 5", nativeQuery=true)
 	public Iterable<HashMap<String,Integer>> findTopCommune();
 	
 	@Query(value = "SELECT crime.crime_name, count(tweet.id_tweet) FROM tweet, crime  " +
-			"WHERE tweet.id_crime = crime.id_crime " +
-			"GROUP BY tweet.id_crime " +
-			"ORDER BY count(tweet.id_tweet) desc " +
-			"LIMIT 5", nativeQuery=true)
+		"WHERE tweet.id_crime = crime.id_crime " +
+		"GROUP BY tweet.id_crime " +
+		"ORDER BY count(tweet.id_tweet) desc " +
+		"LIMIT 5", nativeQuery=true)
 	public Iterable<HashMap<String,Integer>> findTopCrime();
 
 	@Query(value = "SELECT * FROM tweet " + 
-			"WHERE tweet.publication_date LIKE CONCAT(:year,'_','_' '%')",
-			nativeQuery=true)
+		"WHERE tweet.publication_date LIKE CONCAT(:year,'_','_' '%')",
+		nativeQuery=true)
 	public Iterable<Tweet> findTweetsYear(@Param("year") String year);
 	
 	@Query(value = "SELECT * FROM tweet " + 
-			"WHERE tweet.publication_date LIKE CONCAT('_',:month,'_' '%')",
-			nativeQuery=true)
+		"WHERE tweet.publication_date LIKE CONCAT('_',:month,'_' '%')",
+		nativeQuery=true)
 	public Iterable<Tweet> findTweetsMonth( @Param("month") String month);
 
 	@Query(value = "SELECT COUNT(SUBSTRING(tweet.publication_date, 6, 2)), SUBSTRING(tweet.publication_date, 6, 2) FROM tweet " +
-			"GROUP BY (SUBSTRING(tweet.publication_date, 6, 2))", 
-			nativeQuery=true)
+		"GROUP BY (SUBSTRING(tweet.publication_date, 6, 2))", 
+		nativeQuery=true)
 	public  Iterable<HashMap<Integer,String>> findTweetsMonth2();
 
 	@Query(value="SELECT COUNT(SUBSTRING(tweet.publication_date, 6, 2)), "+
@@ -46,36 +46,33 @@ public interface TweetRepository extends PagingAndSortingRepository<Tweet, Integ
 		"GROUP BY (SUBSTRING(tweet.publication_date, 6, 2))",nativeQuery=true)
 	public Iterable<HashMap<Integer,String>> findTweetsCrimeMonth(@Param("id") String id);
 
-	
-
 	@Query(value = "SELECT COUNT(SUBSTRING(tweet.publication_date, 6, 2)), SUBSTRING(tweet.publication_date, 6, 2) FROM tweet " +
-			"WHERE tweet.publication_date LIKE CONCAT(:year,'_','_' '%')" + 
-			"GROUP BY (SUBSTRING(tweet.publication_date, 6, 2))",
-			nativeQuery=true)
+		"WHERE tweet.publication_date LIKE CONCAT(:year,'_','_' '%')" + 
+		"GROUP BY (SUBSTRING(tweet.publication_date, 6, 2))",
+		nativeQuery=true)
 	public Iterable<HashMap<Integer,String>> findTweetsYearMonths(@Param("year") String year);
 
 	@Query(value = "SELECT count(id_tweet) FROM tweet " + 
-  			"WHERE tweet.publication_date LIKE CONCAT(:year,'_',:month '%')",
-  			nativeQuery=true)
-
+		"WHERE tweet.publication_date LIKE CONCAT(:year,'_',:month '%')",
+		nativeQuery=true)
  	public Integer findTweetsDate(@Param("year") String year, @Param("month") String month);
 
  	@Query(value = "SELECT count(id_tweet) FROM tweet " + 
-			"WHERE tweet.publication_date LIKE CONCAT(:year,'_',:month '%')"+
-			"AND tweet.id_crime = :crime",
-			nativeQuery=true)
+		"WHERE tweet.publication_date LIKE CONCAT(:year,'_',:month '%')"+
+		"AND tweet.id_crime = :crime",
+		nativeQuery=true)
 	public Integer findTweetsDateCrime(@Param("year") String year, @Param("month") String month, @Param("crime") Integer crime);	
 
 
 	
 	@Query(value = "SELECT count(id_tweet), 1 FROM tweet " + 
-			"WHERE tweet.publication_date LIKE CONCAT(:year, '-01%')" +
-			"UNION ALL SELECT count(id_tweet), 2 FROM tweet" +
-			"WHERE tweet.publication_date LIKE CONCAT(:year, '-02%')", nativeQuery=true)
+		"WHERE tweet.publication_date LIKE CONCAT(:year, '-01%')" +
+		"UNION ALL SELECT count(id_tweet), 2 FROM tweet" +
+		"WHERE tweet.publication_date LIKE CONCAT(:year, '-02%')", nativeQuery=true)
 	public Iterable<Integer> findTweetsDateAll(@Param("year") String year);
 	
 	@Query(value = "SELECT count(id_tweet) FROM tweet " + 
-			"WHERE tweet.publication_date LIKE CONCAT('%-',:month,'-%')",nativeQuery=true)
+		"WHERE tweet.publication_date LIKE CONCAT('%-',:month,'-%')",nativeQuery=true)
 	public Integer findTweetsGeneralMonth(@Param("month") String month);
 	
 	@Query(value = "SELECT count(id_tweet) FROM tweet",nativeQuery=true)
