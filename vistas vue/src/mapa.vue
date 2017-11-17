2f29446cb3a25b2642586fb37142b6a457e84d29
JorgeParedes77kick <jorge.paredes@usach.cl>
2017-11-17 13:18:41 -0300

    temporal

--- vistas vue/src/mapa.vue
+++ vistas vue/src/mapa.vue
@@ -11,9 +11,11 @@
 
 export default{
   data(){
-        
+        var geoJson;
+        var info = L.control();
         return{
-           
+           geoJson,
+           info
         }
     },
   methods:{
@@ -50,23 +52,80 @@ export default{
     if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
         layer.bringToFront();
       }
+    this.info.update(layer.feature.properties);
     },
 
     resetHighlight:function(e) {
-    geojson.resetStyle(e.target);
+    this.geojson.resetStyle(e.target);
+    this.info.update();
+  },
+
+  zoomToFeature:function(e) {
+    map.fitBounds(e.target.getBounds());
+  },
+
+  onEachFeature: function (feature, layer) {
+    layer.on({
+        mouseover: highlightFeature,
+        mouseout: resetHighlight,
+        click: zoomToFeature
+    });
   }
+
 },
   mounted: function(){
-    var mymap = L.map('mapid').setView([-33.6058007,-70.771973,9], 8.5);
+    var map = L.map('mapid').setView([-33.6058007,-70.771973,9], 8.5);
         L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
           attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
           maxZoom: 18,
           id: 'mapbox.light',
           accessToken: 'pk.eyJ1Ijoiam9yZ2VwYXJlZGVzNzdraWNrIiwiYSI6ImNqYTJxOTJ2cjByZHYzM2xmMmJucnZ6eHYifQ.YQGAKNm1Kk2Nt-4gRuRMQA'
-      }).addTo(mymap);
+      }).addTo(map);
       var mapGeo=require('./comunas_santiago.geo.json');
-      L.geoJson(mapGeo).addTo(mymap);
-      L.geoJson(mapGeo, {style: this.style}).addTo(mymap);
+      L.geoJson(mapGeo).addTo(map);
+      //L.geoJson(mapGeo, {style: this.style}).addTo(map);
+      this.geojson = L.geoJson(statesData, {
+        style: this.style,
+        onEachFeature: this.onEachFeature
+    }).addTo(map);
+
+      var info = L.control();
+
+info.onAdd = function (map) {
+    this._div = L.DomUtil.create('div', 'info'); // create a div with a class "info"
+    this.update();
+    return this._div;
+
+    };
+
+    info.update = function (props) {
+    this._div.innerHTML = '<h4>US Population Density</h4>' +  (props ?
+        '<b>' + props.name + '</b><br />' + props.density + ' people / mi<sup>2</sup>'
+        : 'Hover over a state');
+    };
+
+    this.info.addTo(map);
+
+    var legend = L.control({position: 'bottomright'});
+
+    legend.onAdd = function (map) {
+
+        var div = L.DomUtil.create('div', 'info legend'),
+            grades = [0, 10, 20, 50, 100, 200, 500, 1000],
+            labels = [];
+
+        // loop through our density intervals and generate a label with a colored square for each interval
+        for (var i = 0; i < grades.length; i++) {
+            div.innerHTML +=
+                '<i style="background:' + getColor(grades[i] + 1) + '"></i> ' +
+                grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
+        }
+
+        return div;
+    };
+
+    legend.addTo(map);
+
   }
 }
 </script>
